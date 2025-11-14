2a5df1a3b424058142a17efe95aaa8460f5ebf32
Danilo <danilovitoriastf@gmail.com>
2025-11-14 19:42:08 -0300

    try

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "@/app"
+import { env } from "./env"
 
 
-
-const PORT = process.env.PORT
+const PORT = env.PORT
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
